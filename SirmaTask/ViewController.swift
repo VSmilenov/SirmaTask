6ecaaf909cbf2df5a2ac648b8be944214912ae9d
Vasil Dimitrov <vasil.smilenov@gmail.com>
2024-09-08 17:11:30 +0300

    Initial Commit

---
+++ SirmaTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SirmaTask
+//
+//  Created by Vasil Smilenov on 9/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
